88df45cba67314019daf094a176d8dc7e7401eb4
Raymond Lawson <btzlawson@yahoo.com>
2018-04-14 12:55:40 -0400

    Topic 1 assignments

---
+++ Topic 1/Problem2.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Problem2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("My name is Raymond Lawson.");
+		System.out.println("My favorite color is blue.");
+		System.out.println("If I won the lottery, I would invest most of my money.");
+	}
+
+}
